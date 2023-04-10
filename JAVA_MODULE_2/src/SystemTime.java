b05e3e12e9e9ffe22ea855d13ab0e349ca0fc3ee
hohoang2001 <hohoang1511@gmail.com>
2023-04-10 14:18:29 +0700

    jkhkhhj

--- JAVA_MODULE_2/src/SystemTime.java
+++
@@ -1,7 +0,0 @@
-import java.util.Date;
-public class SystemTime {
-    public static void main(String[] args){
-        Date now = new Date();
-        System.out.printf("Now is: " + now);
-    }
-}
