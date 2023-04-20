e9e9cbb76c98eb37e52e114aabb6a2739ee24d63
hohoang2001 <hohoang1511@gmail.com>
2023-04-20 14:18:57 +0700

    Java

--- JAVA_MODULE_2/src/inheritance/Circle.java
+++ JAVA_MODULE_2/src/inheritance/Circle.java
@@ -9,4 +9,31 @@ public class Circle extends Shape{
         this.radius = radius;
     }
 
+    public Circle(String color, boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea(){
+        return radius * radius * Math.PI;
+    }
+    public double getperimeter(){
+        return 2 * radius * radius * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "A Circle with radius= "
+                + getRadius()
+                + ", which is a subclass of "
+                + super.toString()
+                ;
+    }
 }
